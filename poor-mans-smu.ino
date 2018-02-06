00fc4e1c1f30c176063b34caf71d7f881a2e7bce
unknown <helgelangehaug@hotmail.com>
2018-02-06 23:00:21 +0100

    Simulating live data...

--- poor-mans-smu.ino
+++ poor-mans-smu.ino
@@ -32,7 +32,7 @@ void setup()
 
 void drawMainText() {
 
-  GD.ColorRGB(255,255,255);
+  //GD.ColorRGB(90,89,120);
 
   // put FT81x font 35 in slot 1
   GD.cmd_romfont(1, 34);
@@ -51,12 +51,30 @@ GD.Vertex2ii(10, 30);
   
   GD.ColorRGB(0,255,150);
   GD.ColorA(255);
-  GD.cmd_text(50, 50 ,   1, 0, "+ 15.501 3 V");
+  GD.cmd_text(50, 50 ,   1, 0, "+ 01.501        V");
+    GD.cmd_number(430, 50, 1, 3, random(0, 299));
 
  GD.ColorRGB(0,255,150);
   GD.ColorA(200);
-  GD.cmd_text(50, 150, 29, 0, "SET     15.500 0 V");
-  
+
+GD.Begin(RECTS);
+
+GD.LineWidth(10 * 16); // corner radius 20.0 pixels
+GD.Vertex2ii(40, 160);
+GD.Vertex2ii(410, 185);
+
+  GD.ColorA(255);
+
+   GD.ColorRGB(90,89,120);
+
+  GD.cmd_text(50, 150, 31, 0, "SET     01.500 0 V");
+
+  GD.ColorRGB(232,202,58);
+  GD.ColorA(200);
+
+  GD.cmd_text(550, 150, 31, 0, "%0.");
+  GD.cmd_number(625, 150, 31, 2,  random(0, 20));
+
 
   GD.Begin(LINE_STRIP);
 GD.LineWidth(16);
@@ -68,19 +86,20 @@ GD.Vertex2ii(10, 400);
 GD.Vertex2ii(10, 230);
 
 
-  GD.ColorRGB(0,150,255);
+  GD.ColorRGB(232,202,58);
   GD.ColorA(255);
-  GD.cmd_text(50, 250 ,   1, 0, "+ 0.020 000 A");
+  GD.cmd_text(50, 250 ,   1, 0, "+ 0.020         A");
+  GD.cmd_number(390, 250, 1, 3, random(0, 199));
 
  GD.ColorRGB(0,150,255);
   GD.ColorA(200);
-  GD.cmd_text(50, 350, 29, 0, "LIM     1.000 000 A");
+  GD.cmd_text(50, 350, 31, 0, "LIM     1.000 000 A");
 
 
   
   GD.ColorRGB(50,50,50);
   GD.ColorA(255);
-  GD.cmd_text(10, 440, 28, 0, "Poor Mans Smu");
+  GD.cmd_text(10, 440, 28, 0, "Poor Mans Source Measure Unit");
   GD.cmd_text(10, 460, 27, 0, "by Helge Langehaug");
 
 }
@@ -95,7 +114,7 @@ void loop()
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    GD.ClearColorRGB(0x030303);
+    GD.ClearColorRGB(30, 30, 30);
     GD.Clear();
     drawMainText();
     if (currentMillis - previousMillisSlow >= 10000) {
