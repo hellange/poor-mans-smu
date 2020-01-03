3569c857331cfcbdc80dcfc4d04d89c19add8b65
helge <helgelangehaug@hotmail.com>
2020-01-03 23:58:27 +0100

    Minor adjustment of position and color in pulse display.

--- FunctionPulse.cpp
+++ FunctionPulse.cpp
@@ -139,11 +139,11 @@ void FunctionPulseClass::render(int x, int y) {
   GD.ColorRGB(0x000000);
 
     GD.Tag(PULSE_BUTTON_DEC);
-  GD.cmd_button(x+20,y+40,80,40,28,0,"Dec");
+  GD.cmd_button(x+20,y+50,80,40,28,0,"Dec");
   GD.Tag(0);
   
   GD.Tag(PULSE_BUTTON_INC);
-  GD.cmd_button(x+130,y+40,80,40,28,0,"Inc");
+  GD.cmd_button(x+130,y+50,80,40,28,0,"Inc");
   GD.Tag(0);
 
   y=y+50;
@@ -175,26 +175,41 @@ void FunctionPulseClass::render(int x, int y) {
   }
   
   GD.ColorA(120);
-  GD.cmd_text(x+20, y + 2 ,   29, 0, "SOURCE PULSE");
-  GD.cmd_text(x+20 + 1, y + 2 + 1 ,   29, 0, "SOURCE PULSE");
+  if (operationType == SOURCE_VOLTAGE) {
+    GD.cmd_text(x+20, y + 2 ,   29, 0, "SOURCE VOLTAGE PULSE");
+    GD.cmd_text(x+20 + 1, y + 2 + 1 ,   29, 0, "SOURCE VOLTAGE PULSE");
+  } else {
+    GD.cmd_text(x+20, y + 2 ,   29, 0, "SOURCE CURRENT PULSE");
+    GD.cmd_text(x+20 + 1, y + 2 + 1 ,   29, 0, "SOURCE CURRENT PULSE");
+  }
+  
 
-
+GD.ColorRGB(0xaaaaaa);
   GD.ColorA(255);
   y=y+10;
-  GD.cmd_number(x+242, y, 1, 3, hz);
-  GD.cmd_text(x+410, y ,  1, 0, "Hz");
+  GD.cmd_number(x+242, y+5, 1, 3, hz);
+  GD.cmd_text(x+410, y+5 ,  1, 0, "Hz");
   
   y=y+95;
 //  if (min < 0) {
 //    GD.cmd_text(x+220, y ,  31, hz, "-");
 //  }
 //  GD.cmd_number(x+242, y, 31, 4, abs(min));
+
+  
+  
   DIGIT_UTIL.renderValue(x + 220,  y, min, 4, operationType == SOURCE_VOLTAGE ? DigitUtilClass::typeVoltage : DigitUtilClass::typeCurrent); 
 
+if (operationType == SOURCE_VOLTAGE) {
+    GD.ColorRGB(COLOR_VOLT);
+  } else {
+    GD.ColorRGB(COLOR_CURRENT);
+  }
+  
   //GD.cmd_text(x+365, y ,  31, 0, operationType == SOURCE_VOLTAGE ? "mV" : "mA");
   DIGIT_UTIL.renderValue(x + 500,  y, measuredLow, 4, operationType == SOURCE_VOLTAGE ? DigitUtilClass::typeVoltage : DigitUtilClass::typeCurrent); 
 
-
+  GD.ColorRGB(0xaaaaaa);
   y=y+45;
 //  if (max < 0) {
 //    GD.cmd_text(x+220, y ,  31, 0, "-");
@@ -203,6 +218,11 @@ void FunctionPulseClass::render(int x, int y) {
 
   DIGIT_UTIL.renderValue(x + 220,  y, max, 4, operationType == SOURCE_VOLTAGE ? DigitUtilClass::typeVoltage : DigitUtilClass::typeCurrent); 
 
+if (operationType == SOURCE_VOLTAGE) {
+    GD.ColorRGB(COLOR_VOLT);
+  } else {
+    GD.ColorRGB(COLOR_CURRENT);
+  }
   //GD.cmd_text(x+365, y ,  31, 0, operationType == SOURCE_VOLTAGE ? "mV" : "mA");
   DIGIT_UTIL.renderValue(x + 500,  y, measuredHigh, 4, operationType == SOURCE_VOLTAGE ? DigitUtilClass::typeVoltage : DigitUtilClass::typeCurrent); 
 
